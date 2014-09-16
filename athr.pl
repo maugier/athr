0fd8c8af9243dc3a0b023bba9611318749926b12
Maxime Augier <max@xolus.net>
2014-09-16 15:28:06 +0200

    Tweaking

--- athr.pl
+++ athr.pl
@@ -10,19 +10,26 @@ my $hostserv = "HostServ";
 
 #== CONFIG END==
 
-my $vhosts;
 my $nicks;
+my $chans;
 
-my $nick_re = qr/[^ ]+/;
+my $nick_re = qr/[^\s]+/;
 my $vhost_re = qr/[a-zA-Z0-9.-]+/;
-my $email_re = qr/([^\s])+/;
+my $email_re = qr/[^\s]+/;
+my $chan_re = qr/[^\s]+/;
 
 while(<>) {
-    if (m/$hostserv> ($nick_re) ACTIVATE ($vhost_re) for ($nick_re)/o) {
-        $vhosts->{$3}{$2} = 1;    
+    if (m/$hostserv> ($nick_re) ACTIVATE: ($vhost_re) for ($nick_re)/o) {
+        $nicks->{$3}{'vhosts'}{$2} = 1;    
     } elsif (m/$nickserv> ($nick_re) REGISTER: ($nick_re) to ($email_re)/o) {
-        $nicks->{$2} = $3
-    } 
+        $nicks->{$2}{'email'} = $3
+    } elsif (m/$nickserv> ($nick_re) \(($nick_re)\) GROUP: ($nick_re) to ($nick_re)/o) {
+        $nicks->{$4}{'grouped'}{$4} = 1;
+    } elsif (m/$nickserv> ($nick_re) F?DROP: ($nick_re)/o) {
+        delete $nicks->{$2};
+    } elsif (m/$chanserv> ($nick_re) DROP: ($chan_re)/o) {
+        delete $chans->{$2};
+    }
 }
 
-print Dumper({ nicks => $nicks, vhosts => $vhosts });
+print Dumper({ nicks => $nicks, chans => $chans });
